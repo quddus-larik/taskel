028c81b8d8e966ff23260f548116d589e525ff89
quddus larik <tg53rt3@gmail.com>
2025-09-06 17:09:40 +0500

    security for routes

---
+++ client/src/components/providers/route-guards.tsx
@@ -0,0 +1,21 @@
+import { Navigate } from "react-router"
+import { useAuth } from "@/hooks/use-auth"
+import type { JSX } from "react"
+
+export function ProtectedRoute({ children }: { children: JSX.Element }) {
+    const { user, loading } = useAuth()
+
+    if (loading) return <div>Loading...</div>
+    if (!user) return <Navigate to="/login" replace />
+
+    return children
+}
+
+export function GuestRoute({ children }: { children: JSX.Element }) {
+  const { user, loading } = useAuth()
+
+  if (loading) return <div>Loading...</div>
+  if (user) return <Navigate to="/dashboard" replace />
+
+  return children
+}
